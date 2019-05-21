ff8eef0e90d3f67e2d4c6743e5cdbf0d9de6a0d3
jonathan cruz gaona <jcruz2@udi.edu.co>
2019-05-20 21:28:57 -0500

    creacion de entidades

--- src/JC/UserBundle/Controller/UserController.php
+++ src/JC/UserBundle/Controller/UserController.php
@@ -6,10 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
-{
+{ 
     public function indexAction()
     {
-        return new Response('Bienvenido a mi modulo de Usuarios');
+       $em =$this->getDoctrine()->getManager();
+
+       $users= $em->getRepository('JCUserBundle:User')->findAll();
+       $res = 'Lista de usuarios : <br/>';
+
+       foreach ($users as $user) {
+     
+        $res .= 'Usuario: ' .$user->getUsername(). ' - Email: '.$user->getEmail(). '<br />';
+       }
+       return new Response($res);
+    }
+
+    public function viewAction($id){
+
+    	$repository=$this->getDoctrine()->getRepository('JCUserBundle:User');
+
+    	$user= $repository->find($id);
+    	return new Response(' Usuario: '. $user->getUsername().' con email: '. $user->getEmail());
     }
     
 }
